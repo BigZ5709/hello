3ece87d89a0bd0b1711e657bd71e813d8ee87279
FaisalUNL <falmaaini2@huskers.unl.edu>
2021-08-27 20:37:57 +0000

    added my own account

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
- * Author: Zaiden De La O
- *         zdelao3@huskers.unl.edu
- * Date: 2021/08/26
+ * Author: Zaiden De La O / Faisal Al Maaini
+ *         zdelao3@huskers.unl.edu / falmaaini2@huskers.unl.edu
+ * Date: 2021/08/27
  *
  * My name and major in c
  *
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Zaiden De La O!\n");
   printf("Undeclared Major!\n");
+  printf("Faisal Maaini!\n");
+  printf("Computer Science\n");
 
   return 0;
 
